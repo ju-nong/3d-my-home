abbd229ac11210cecd232f1b7febeb19d52e3713
이준용 <iiiuooyoo@gmail.com>
2023-04-03 23:33:28 +0900

    feat : 벽 + 침대

---
+++ src/pages/main.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const MainStyled = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    position: relative;
+`;
+
+function Main() {
+    return <MainStyled>오 안녕하세요</MainStyled>;
+}
+
+export { Main as default };
